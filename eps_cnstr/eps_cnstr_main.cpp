7723e57a97fe82cf341eef6b11352d59d261fb3b
Ritam Guha <ritamguha16@gmail.com>
2025-06-11 22:45:48 -0400

    bug(pso_exec_uuid)

--- eps_cnstr/eps_cnstr_main.cpp
+++ eps_cnstr/eps_cnstr_main.cpp
@@ -116,7 +116,7 @@ int main(
     }
     */
     if (option == 1) { //ipopt Opt3
-        EpsConstraint eps_constr(base_scenario_json, scenario_json, path_out, pollutant_idx, evaluate_cast, exec_path, base_scenario_uuid, base_scenario_str);
+        EpsConstraint eps_constr(base_scenario_json, scenario_json, path_out, pollutant_idx, evaluate_cast, exec_path, base_scenario_uuid, base_scenario_str, pso_exec_uuid);
         eps_constr.constr_eval(reduction, nsteps, uuids, parent_uuid_path);
     }
     /*
